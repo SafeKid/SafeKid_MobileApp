1efffcc9fedf6e486ad6f4b2bf82baf091d7ea53
Hasi456 <hasichnd97@gmail.com>
2020-06-30 18:31:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SafeKid_MobileApp
+ 
---
+++ react-navigation-v5-mix-master/screens/MainTabScreen.js
@@ -0,0 +1,147 @@
+import React from 'react';
+
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import HomeScreen from './HomeScreen';
+import DetailsScreen from './DetailsScreen';
+import ExploreScreen from './ExploreScreen';
+import ProfileScreen from './ProfileScreen';
+
+const HomeStack = createStackNavigator();
+const DetailsStack = createStackNavigator();
+const ProfileStack= createStackNavigator();
+const ExploreStack=createStackNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MainTabScreen = () => (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#fff"
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarColor: '#585858',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={DetailsStackScreen}
+        options={{
+          tabBarLabel: 'Notifications',
+          tabBarColor: '#585858',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-notifications" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileStackScreen}
+        options={{
+          tabBarLabel: 'News Feed',
+          tabBarColor: '#585858',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-paper" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Explore"
+        component={ExploreStackScreen}
+        options={{
+          tabBarLabel: 'Location',
+          tabBarColor: '#585858',
+          tabBarIcon: ({ color }) => (
+            <Icon name="ios-pin" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+);
+
+export default MainTabScreen;
+
+const HomeStackScreen = ({navigation}) => (
+<HomeStack.Navigator screenOptions={{
+        headerStyle: {
+        backgroundColor: '#585858',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+        fontWeight: 'bold'
+        }
+    }}>
+        <HomeStack.Screen name="Home" component={HomeScreen} options={{
+        title:'Overview',
+        headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor="#585858" onPress={() => navigation.openDrawer()}></Icon.Button>
+        )
+        }} />
+</HomeStack.Navigator>
+);
+
+const DetailsStackScreen = ({navigation}) => (
+<DetailsStack.Navigator screenOptions={{
+        headerStyle: {
+        backgroundColor: '#585858',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+        fontWeight: 'bold'
+        }
+    }}>
+        <DetailsStack.Screen name="Notifications" component={DetailsScreen} options={{
+        headerLeft: () => (
+            <Icon.Button name="ios-menu" size={25} backgroundColor="#585858" onPress={() => navigation.openDrawer()}></Icon.Button>
+        )
+        }} />
+</DetailsStack.Navigator>
+);
+
+const ProfileStackScreen = ({navigation}) => (
+  <ProfileStack.Navigator screenOptions={{
+          headerStyle: {
+          backgroundColor: '#585858',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+          fontWeight: 'bold'
+          }
+      }}>
+          <ProfileStack.Screen name="News Feed" component={ProfileScreen} options={{
+          headerLeft: () => (
+              <Icon.Button name="ios-menu" size={25} backgroundColor="#585858" onPress={() => navigation.openDrawer()}></Icon.Button>
+          )
+          }} />
+  </ProfileStack.Navigator>
+  );
+  
+  const ExploreStackScreen = ({navigation}) => (
+    <ExploreStack.Navigator screenOptions={{
+            headerStyle: {
+            backgroundColor: '#585858',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+            fontWeight: 'bold'
+            }
+        }}>
+            <ExploreStack.Screen name="Location" component={ExploreScreen} options={{
+            headerLeft: () => (
+                <Icon.Button name="ios-menu" size={25} backgroundColor="#585858" onPress={() => navigation.openDrawer()}></Icon.Button>
+            )
+            }} />
+    </ExploreStack.Navigator>
+    );
+      
+  
